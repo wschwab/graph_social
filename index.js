496bfea89218c086c930feaa7445019bd3fe4b8d
wschwab <wschwab@protonmail.com>
2020-10-19 15:23:10 +0300

    added .env, connect to Mongo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 const { ApolloServer } = require('apollo-server');
 // following needed for types
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
 
 const typeDefs = gql`
     type Query{
@@ -19,7 +22,11 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port: 5555 })
+mongoose.connect(`mongodb+srv://William:${process.env.MONGO_PASS}@cluster0.oniyx.mongodb.net/<dbname>?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected');
+        return server.listen({ port: 5555 })
+    })
     .then(res => {
         console.log(`server running at ${res.url}`)
     })
