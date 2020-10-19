18cf41c87bc14fbe79dc564ddee12b34d6aecc89
wschwab <wschwab@protonmail.com>
2020-10-19 15:41:08 +0300

    added models, can query db w/graphql

--- index.js
+++ index.js
@@ -5,24 +5,39 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
+
 const typeDefs = gql`
-    type Query{
-        sayHi: String!
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
     }
-`
+    type Query{
+        getPosts: [Post]
+    }
+`;
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
     }
-}
+};
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-mongoose.connect(`mongodb+srv://William:${process.env.MONGO_PASS}@cluster0.oniyx.mongodb.net/<dbname>?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://William:${process.env.MONGO_PASS}@cluster0.oniyx.mongodb.net/graph_social?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB connected');
         return server.listen({ port: 5555 })
